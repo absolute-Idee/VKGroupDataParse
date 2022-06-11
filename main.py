487f704120f49eb3b421eb51b15cb336a171dba1
maxim <urbaevmax@gmail.com>
2022-06-11 19:26:04 +0300

    sex sample added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from users_info import get_json, collect_info, get_users, write_csv
-from post_info import getjson, get_all_posts, write_2csv, make_posts
+#from post_info import getjson, get_all_posts, write_2csv, make_posts
 
 group_id = '108291890'
 token = ''
@@ -9,11 +9,11 @@ def write_users_info_csv(token, group_id):
     info = collect_info(users, group_id)
     write_csv(info)
 
-def write_posts_info_csv(token, group_id):
-    all_posts, count_posts = get_all_posts (token, group_id)
-    pposts = make_posts(all_posts)
-    write_2csv(pposts)
+# def write_posts_info_csv(token, group_id):
+#     all_posts, count_posts = get_all_posts (token, group_id)
+#     pposts = make_posts(all_posts)
+#     write_2csv(pposts)
 
 if __name__=='__main__':
-    #write_users_info_csv(token=token, group_id=group_id)
-    write_posts_info_csv(token, group_id)
+    write_users_info_csv(token=token, group_id=group_id)
+    #write_posts_info_csv(token, group_id)
--- users_info.py
+++ users_info.py
@@ -49,11 +49,19 @@ def collect_info(users, group_id):
             city = user['city']['title']
         except:
             city = ''
+        try:
+            if user['sex'] == 1:
+                sex = 'female'
+            elif user['sex'] == 2:
+                sex = 'male'
+        except:
+            sex = ''
 
         filtered_user = {
                 'id': id,
                 'bdate': bdate,
                 'city': city,
+                'sex': sex,
                 'group': group_id
             }
     
@@ -65,7 +73,7 @@ def write_csv(data, encoding = 'utf-8'):
     group_id = data[0]['group']
     filename = f'{group_id}_users_info.csv'
     with open(filename, 'w', newline = '', encoding = encoding) as csvfile:
-        fieldnames = ['id','bdate', 'city', 'group']
+        fieldnames = ['id', 'sex', 'bdate', 'city', 'group']
         writer = csv.DictWriter(csvfile, delimiter = ';', fieldnames = fieldnames, extrasaction = 'ignore')
         writer.writeheader()
         writer.writerows(data)
